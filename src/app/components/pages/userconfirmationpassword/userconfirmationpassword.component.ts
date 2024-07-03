353dfa1dc3ec1f6f3e56a8b5616304303f4647a4
gizellemanu <gizellemanu@hotmail.com>
2024-07-03 11:25:35 -0300

    certificado

--- src/app/components/pages/userconfirmationpassword/userconfirmationpassword.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-userconfirmationpassword',
-  templateUrl: './userconfirmationpassword.component.html',
-  styleUrls: ['./userconfirmationpassword.component.css']
-})
-export class UserconfirmationpasswordComponent {}
